2fec78a31d7032a952c41345b1858b1d0af8c372
jeffeliu <xiaoniu909@126.com>
2018-11-16 20:42:31 +0800

    添加文本回包内容

--- mid/e.js
+++ mid/e.js
@@ -25,5 +25,7 @@ module.exports = function (req, res, next) {
     const sign = com.sign(JSON.stringify(response), key);
     const data = com.encrypt(JSON.stringify(response), key, vi, 0);
 
+    res.set('Content-Type', 'text/plain');
+
     return res.send(`d=${encodeURIComponent(data)}&s=${encodeURIComponent(sign)}&k=${client}`);
 };
